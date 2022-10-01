73b98bd50a161c1efd7ace99ee2032ad885ab802
manwithoutsleep <brad@southcountysound.com>
2022-10-01 15:44:36 -0500

    feat: Fix SimplePractice widget
    Remove unused PHP files

--- includes/includes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once ( 'includes/constants.php' );
-include_once ( 'includes/functions.php' );
-include_once ( 'includes/header.php'    );
-include_once ( 'includes/footer.php'    );
-
-?>
